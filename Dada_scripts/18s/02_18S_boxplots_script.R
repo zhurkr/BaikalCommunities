b29863ba3ab2f7c376592660dc908adc0cd4a768
OtterLawyer <130027058+OtterLawyer@users.noreply.github.com>
2024-05-26 05:47:57 +0900

    Update 02_18S_boxplots_script.R

--- Dada_scripts/18s/02_18S_boxplots_script.R
+++ Dada_scripts/18s/02_18S_boxplots_script.R
@@ -1,9 +1,9 @@
 require(phyloseq)
 require(ggplot2)
 
-p <- plot_richness(ps_na, x = "Month", measures=c("ACE", "Shannon", "Simpson", "Chao1"))
+p <- plot_richness(ps_na, x = "Month", measures=c("ACE", "Shannon", "Simpson"))
 
-boxplots <- p + geom_boxplot(aes(colour = Month)) + 
+boxplots <- p + geom_boxplot(aes(colour = Month, fill=Month)) + 
   labs(title = "18S_275", x = "Month", y = "Alpha diversity measure") + 
   theme_minimal() +
   geom_point(position = position_jitter(width = 0.2), size = 1.5, alpha = 0.5) +
