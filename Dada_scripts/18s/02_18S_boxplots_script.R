9f83629353bc2cac76e1d9bb96418d7fbf7ed204
OtterLawyer <130027058+OtterLawyer@users.noreply.github.com>
2024-05-26 04:00:56 +0900

    rename 18s scripts

---
+++ Dada_scripts/18s/02_18S_boxplots_script.R
@@ -0,0 +1,11 @@
+require(phyloseq)
+require(ggplot2)
+
+p <- plot_richness(ps_na, x = "Month", measures=c("ACE", "Shannon", "Simpson", "Chao1"))
+
+boxplots <- p + geom_boxplot(aes(colour = Month)) + 
+  labs(title = "18S_275", x = "Month", y = "Alpha diversity measure") + 
+  theme_minimal() +
+  geom_point(position = position_jitter(width = 0.2), size = 1.5, alpha = 0.5) +
+  theme(plot.title = element_text(hjust = 0.5, size = 15), axis.title.x = element_text(size = 20), axis.title.y = element_text(size = 20), axis.text.x = element_text(size = 20))
+ggsave("18s_boxplots_275.jpeg", plot = boxplots, width = 10, height = 6, dpi = 300)
