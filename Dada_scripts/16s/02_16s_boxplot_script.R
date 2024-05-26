8522c7edbee86d03d15984c5bf62056010b6942a
OtterLawyer <130027058+OtterLawyer@users.noreply.github.com>
2024-05-26 10:20:25 +0900

    Update 02_16s_boxplot_script.R

--- Dada_scripts/16s/02_16s_boxplot_script.R
+++ Dada_scripts/16s/02_16s_boxplot_script.R
@@ -1,8 +1,8 @@
 require(phyloseq)
 require(ggplot2)
 
-ps_nc_FL <- subset_samples(ps_16s_nc, Lifestyle == "Free-Living")
-ps_nc_PA <- subset_samples(ps_16_nc, Lifestyle == "Particle attached")
+ps_nc_FL <- subset_samples(ps_nc, Lifestyle == "Free-Living")
+ps_nc_PA <- subset_samples(ps_nc, Lifestyle == "Particle attached")
 
 # ACE, Shannon, Simpson, Chao1
 ## FL
@@ -13,7 +13,8 @@ boxplots_FL <- p + geom_boxplot(aes(colour = Month, fill = Month)) +
   theme_minimal() +
   geom_point(position = position_jitter(width = 0.2), size = 1.5, alpha = 0.5) +
   theme(plot.title = element_text(hjust = 0.5, size = 15), axis.title.x = element_text(size = 13), axis.title.y = element_text(size = 13), axis.text.x = element_text(size = 13),
-        legend.position = "none") 
+        legend.position = "none")
+print(boxplots_FL)
 ggsave("boxplots_275_FL.jpeg", plot = boxplots_FL, width = 10, height = 6, dpi = 300)
 
 ## PA
@@ -24,7 +25,8 @@ boxplots_PA <- p + geom_boxplot(aes(colour = Month, fill = Month)) +
   theme_minimal() +
   geom_point(position = position_jitter(width = 0.2), size = 1.5, alpha = 0.5) +
   theme(plot.title = element_text(hjust = 0.5, size = 15), axis.title.x = element_text(size = 13), axis.title.y = element_text(size = 13), axis.text.x = element_text(size = 13),
-        legend.position = "none")      
+        legend.position = "none")
+print(boxplots_PA)
 ggsave("boxplots_275_PA.jpeg", plot = boxplots_PA, width = 10, height = 6, dpi = 300)
 
 
